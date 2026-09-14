151886748175ff97c831df67587ae0327509525f
benson-e <90936042+FTC-Photon@users.noreply.github.com>
2026-09-14 10:19:20 -0500

    turret and drive
    learning how to create PID and a check for the field centric drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Turret.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode.Mechanisms;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+public class Turret {
+    private DcMotor turret;
+    private double kP = 0.0001;
+    private double kD = 0.0000;
+    private double goalX = 0;
+    private double lastError = 0;
+    private double angleTolerance = 0.2;
+    private final double MAX_POWER = 0.6;
+    private double power = 0;
+    private final ElapsedTime timer = new ElapsedTime();
+
+    public void init(HardwareMap hwMap){
+        turret = hwMap.get(DcMotor.class, "turret");
+        turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void setkp (double newKP){
+        kP= newKP;
+    }
+
+    public double getkP(){
+        return kP;
+    }
+
+    public void setkD (double newKD){
+        kD= newKD;
+    }
+
+    public double getkD(){
+        return kD;
+    }
+
+    public void resetTimer(){
+        timer.reset();
+    }
+
+    public void update(LLResult llresult ){
+        double deltaTime = timer.seconds();
+        timer.reset();
+
+        if (llresult == null){
+            turret.setPower(0);
+            lastError = 0;
+        }
+
+        //________ Start PD CONTROLLER________
+
+        double error = goalX - llresult.getTx();
+        double pTerm = error * kP;
+
+        double dTerm = 0;
+        if (deltaTime > 0){
+            dTerm = (error - lastError)/(deltaTime * kD);
+        }
+
+        if(Math.abs(error)< angleTolerance){
+            power = 0;
+        } else {
+            power = Range.clip(pTerm + dTerm, -MAX_POWER, MAX_POWER);
+
+        }
+
+        // SAFETIES TO MAKE SURE YOU'RE NOT ROTATING PAST
+
+        turret.setPower(power);
+        lastError = error;
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldOriented.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Mechanisms.MecanumDrive;
+
+@TeleOp
+public class FieldOriented extends OpMode {
+    MecanumDrive drive = new MecanumDrive();
+    double forward, strafe, rotate;
+    @Override
+    public void init() {
+        drive.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        forward = gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        drive.driveFieldRelative(forward,strafe,rotate);
+    }
+}
